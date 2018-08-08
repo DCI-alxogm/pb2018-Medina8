edf3d48720328a1e17d4fcca2b9970e303a39760
Medina Maldonado Paloma Guadalupe <medinamp2018@dci25-152.ugto.mx>
2018-08-08 09:36:12 -0500

    Cree mi primer programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,11 @@
+/*creado el 8 de agosto del 2018 por Paloma Medina */
+
+#include<stdio.h>
+int main()
+{
+	printf ("Hola Paloma\n");
+	printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
+
