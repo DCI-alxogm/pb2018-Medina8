b44248666735080ed3a1fc392259bf2bd6b8edf7
Medina Maldonado Paloma Guadalupe <medinamp2018@dci25-152.ugto.mx>
2018-08-16 14:39:44 -0500

    Agregue los ejercicios

---
+++ semana2/programa_ejercicio2.c
@@ -0,0 +1,20 @@
+/*Creado el 15 de agosto de 2018 por Paloma Medina*/
+#include<stdio.h>
+  int main()
+  {
+        int a,b,c,d,e;
+        printf("Introduzca 4 numeros enteros \n");
+	scanf("%i %i %i %i", &a, &b, &c,&d);
+        e=(a+b)*c/d;
+	printf("El valor de la operacion e=(a+b)*c/d es %i \n", e);
+	e=((a+b)*c)/d;
+	printf("El valor de la operacion e=((a+b)*c)/d es %i \n", e);
+        e=(a+b)*c/d;
+        printf("El valor de la operacion e=(a+b9*c/d es %i \n" , e);
+	e=a+(b*c)/d;
+	printf("El valor de la operacion e=a+(b*C)/d es %i \n" ,e);
+
+        return 0;
+  }	
+
+				 
